08417cfc9f05273bfda8e76ba9811211d92af17b
shilangyu <xmarcinmarcin@gmail.com>
2022-11-06 07:55:35 +0100

    Add theme context

---
+++ web/src/stores/ThemeContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, FC, ReactNode, useContext, useState } from 'react'
+
+export enum ThemeMode {
+    system,
+    light,
+    dark,
+}
+
+export enum ResolvedTheme {
+    light,
+    dark,
+}
+
+const ThemeContext = createContext<{
+    mode: ResolvedTheme
+    changeMode: (mode: ThemeMode) => void
+}>(null!)
+
+export const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+    const [mode, setMode] = useState(ThemeMode.system)
+
+    const resolved = (() => {
+        switch (mode) {
+            case ThemeMode.system:
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    return ResolvedTheme.dark
+                } else {
+                    return ResolvedTheme.light
+                }
+            case ThemeMode.light:
+                return ResolvedTheme.light
+            case ThemeMode.dark:
+                return ResolvedTheme.dark
+        }
+    })()
+
+    return (
+        <ThemeContext.Provider value={{ mode: resolved, changeMode: setMode }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useThemeContext = () => useContext(ThemeContext)
