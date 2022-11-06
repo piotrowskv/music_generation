29a45d7ec34e0e84bc94f4f4be3b01a7805148f9
shilangyu <xmarcinmarcin@gmail.com>
2022-11-06 08:38:47 +0100

    Add theme switching

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,9 +8,9 @@ function App() {
 
     return (
         <div className={clsx(mode === ResolvedTheme.dark && 'dark')}>
-            <div className="flex h-screen w-screen bg-white p-4 text-black dark:bg-black dark:text-white">
+            <div className="flex h-screen w-screen bg-white p-4 text-black  dark:bg-black dark:text-white">
                 <h1 className="text-3xl font-bold">Music generation</h1>
-                <MusicNote className="dark:fill-white" />
+                <MusicNote className="h-9 w-9 dark:fill-white" />
                 <div className="flex-1"></div>
 
                 <ThemeSwitch />
--- web/src/components/ThemeSwitch.tsx
+++ web/src/components/ThemeSwitch.tsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import Moon from '../assets/Moon'
+import Sun from '../assets/Sun'
+import {
+    ResolvedTheme,
+    ThemeMode,
+    useThemeContext,
+} from '../stores/ThemeContext'
 
 interface Props {}
 
-const ThemeSwitch: React.FC<Props> = ({}) => <div></div>
+const ThemeSwitch: React.FC<Props> = ({}) => {
+    const { mode, changeMode } = useThemeContext()
+
+    let Icon = mode === ResolvedTheme.dark ? Moon : Sun
+
+    return (
+        <button
+            className="h-9 w-9"
+            onClick={() =>
+                changeMode(
+                    mode === ResolvedTheme.dark
+                        ? ThemeMode.light
+                        : ThemeMode.dark
+                )
+            }
+        >
+            <Icon className="h-9 w-9 dark:fill-white" />
+        </button>
+    )
+}
 
 export default ThemeSwitch
