e08ccc7f9958d34bbced8986ced339c195728830
shilangyu <xmarcinmarcin@gmail.com>
2022-11-06 07:55:05 +0100

    Add custom theming

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        colors: {
+            white: '#ffffff',
+            black: '#292929',
+        },
         extend: {},
     },
     plugins: [],
+    darkMode: 'class',
 }
