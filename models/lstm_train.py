4c59a42af7e7e70c6bf496f8fceed31751109d6c
szgorski <101061451+szgorski@users.noreply.github.com>
2022-11-30 01:17:20 +0100

    Update lstm_train.py

--- models/lstm_train.py
+++ models/lstm_train.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, Activation, BatchNormalization
 from keras.callbacks import ModelCheckpoint
@@ -34,23 +35,35 @@ def get_network(input_size):
     model = Sequential()
     model.add(LSTM(
         512,
-        batch_input_shape=input_size,    # should be set up differently?
-        recurrent_dropout=0.3,
+        input_shape=(input_size[1], input_size[2]),
         return_sequences=True
+        # recurrent_dropout=0.3  - will not run on GPU if not separated
     ))
-    model.add(LSTM(
-        512,
-        return_sequences=True,
-        recurrent_dropout=0.3
-    ))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.02))
+
+    # model.add(LSTM(
+    #     512,
+    #     return_sequences=True,
+    #     # recurrent_dropout=0.3
+    # ))
+    # model.add(BatchNormalization())
+    # model.add(Dropout(0.3))
+
     model.add(LSTM(512))
     model.add(BatchNormalization())
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.02))
+
     model.add(Dense(256))
-    model.add(Activation('relu'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.02))
+
     model.add(Dense(128))
+    model.add(Activation('relu'))    # model.add(Activation('relu' | 'tanh'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.02))
+
+    model.add(Dense(64))
     model.add(Activation('softmax'))
     model.compile(
         loss='categorical_crossentropy',
@@ -73,8 +86,8 @@ def train_network(lstm_model, lstm_input, lstm_output):
     lstm_model.fit(
         lstm_input,
         lstm_output,
-        epochs=25,
-        batch_size=len(lstm_input),  # should be set up differently?
+        epochs=200,         # TODO: 200
+        batch_size=512,     # TODO: 128
         callbacks=callbacks_list
     )
 
@@ -89,7 +102,7 @@ if __name__ == '__main__':
         path = os.path.join('../data', name)
 
         try:
-            # midi_input = np.load(path, allow_pickle=True)  # if .npy files loaded
+            # midi_input = np.load(path, allow_pickle=True)  # if .npy files loadeds
             midi_input = get_sequence_of_notes(path, Mode.BOOLEANS, True, False)
             notes_sequences, lengths_sequences, notes_predictions, lengths_predictions = get_sequences(midi_input)
             notes_input.extend(notes_sequences)
@@ -99,9 +112,39 @@ if __name__ == '__main__':
             # print(f'        loaded {name}')
 
         except Exception as ex:
-            pass
-            # print(f'failed to load {name}')
+            # pass
+            print(f'failed to load {name}')
 
-    input_size = (len(notes_input), 100, 128)
+    ni = np.asarray(notes_input)
+    ni = ni[:, :, 23:87]  # 27 - 86 in use, 22 - 86 to round to 64
+    no = np.asarray(notes_output)
+    no = no[:, 23:87]
+
+    # for i in range(len(notes_sequences)):
+    #     for j in range(100):
+    #         notes_sequences[i][j] = notes_sequences[i][j][27:87]
+    #         notes_sequences[i][j][27] = False
+    #
+    # for i in range(len(notes_predictions)):
+    #     notes_predictions[i] = notes_predictions[i][27:87]
+    #     notes_predictions[i][27] = False
+
+    # k_min = 128
+    # k_max = 0
+    # for num, s in enumerate(notes_input):
+    #     print(num)
+    #     for keys in s:
+    #         for pos, value in enumerate(keys):
+    #             if value:
+    #                 if pos < k_min:
+    #                     k_min = pos
+    #                 if pos > k_max:
+    #                     k_max = pos
+    #
+    # print()
+    # print(k_min)
+    # print(k_max)
+
+    input_size = (len(ni), SEQUENCE_LENGTH, 64)  # notes_input
     model = get_network(input_size)
-    train_network(model, notes_input, notes_output)
+    train_network(model, ni, no)   # notes_input, notes_output
