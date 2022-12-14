b538afb5095bb0a260f32d76f4576fa97d18c10b
szgorski <101061451+szgorski@users.noreply.github.com>
2022-12-14 22:57:09 +0100

    added alpha version of polyphony-LSTM

---
+++ midi/music_21.py
@@ -0,0 +1,80 @@
+import os
+import numpy as np
+
+from music21 import *
+
+f = np.load('../sequences/fugue1.npy', allow_pickle=True)
+f = f.tolist()
+
+events = [[] for _ in range((len(f) + 1))]
+tuples = [None] * len(f)
+
+active_tracks = range(len(f))
+active_tracks = list(active_tracks)
+to_remove = []
+removed_tracks = []
+
+for track in active_tracks:
+    if len(f[track]) > 0:
+        tuples[track] = f[track].pop(0)
+    else:
+        to_remove.append(track)
+        # active_tracks.remove(track)
+
+for track in to_remove:
+    active_tracks.remove(track)
+    removed_tracks.append(track)
+to_remove.clear()
+
+while len(active_tracks) > 0:
+    d = [tuples[i][0] for i in active_tracks]
+    diff = min(d)
+    events[len(f)].append(diff)
+
+    for track in active_tracks:
+        events[track].append(tuples[track][1])
+        tuples[track] = (tuples[track][0] - diff, tuples[track][1])
+
+        while tuples[track][0] == 0:
+            if len(f[track]) == 0:
+                to_remove.append(track)
+                break
+            else:
+                tuples[track] = f[track].pop(0)
+
+    for track in removed_tracks:
+        events[track].append([])
+
+    for track in to_remove:
+        active_tracks.remove(track)
+        removed_tracks.append(track)
+    to_remove.clear()
+
+features = [[] for _ in range((2 * len(f) + 2))]
+for track in range(len(events) - 1):
+    while len(events[track]) > 0:
+        state = events[track].pop(0)
+
+        if len(state) == 0:
+            features[2 * track + 0].append(0.0)
+            features[2 * track + 1].append(0.0)
+        elif len(state) == 1:
+            features[2 * track + 0].append((state[0] // 12 - 1) / 8)    # octaves mapping x/8
+            features[2 * track + 1].append((state[0] % 12 + 1) / 12)    # tones mapping x/12
+        else:
+            ch = chord.Chord(state)
+            ch = ch.root().midi
+            features[2 * track + 0].append((ch // 12 - 1) / 8)
+            features[2 * track + 1].append((ch % 12 + 1) / 12)
+
+while len(events[-1]) > 0:
+    state = events[-1].pop(0)
+    features[-2].append(1 / state) # lengths mapping 1/x
+    features[-1].append(0.0) # TODO: tempos + mapping 1000/x
+
+# for each note list determine the main note, if 0-1-N, calc features and put to the array
+# collect tempo array and iterate through last list += offset to get tempo array positions
+
+# output = np.asarray(features)
+os.makedirs('../sequences', exist_ok=True)
+np.save('../sequences/chords21', features)
--- models/lstm_train.py
+++ models/lstm_train.py
@@ -1,6 +1,6 @@
 import os
-
 import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, Activation, BatchNormalization
 from keras.callbacks import ModelCheckpoint
@@ -9,29 +9,141 @@ from midi.midi import get_sequence_of_notes, Mode
 SEQUENCE_LENGTH = 100
 
 
-def get_sequences(raw_input):
-    notes_sequences = []
-    lengths_sequences = []
-    notes_predictions = []
-    lengths_predictions = []
-
-    for i in range(SEQUENCE_LENGTH, len(raw_input)):
-        notes_sequence = []
-        lengths_sequence = []
-
-        for index in range(i - SEQUENCE_LENGTH, i):
-            notes_sequence.append(raw_input[index][1])
-            lengths_sequence.append(raw_input[index][0])
-        notes_sequences.append(notes_sequence)
-        lengths_sequences.append(lengths_sequence)
-
-        notes_predictions.append(raw_input[i][1])
-        lengths_predictions.append(raw_input[i][0])
-
-    return notes_sequences, lengths_sequences, notes_predictions, lengths_predictions
+def train_network(lstm_model, lstm_input, lstm_output):
+    filepath = 'weights.hdf5'
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    callbacks_list = [checkpoint]
+    lstm_model.fit(
+        lstm_input,
+        lstm_output,
+        epochs=200,         # TODO: 200
+        batch_size=32,      # TODO: 128
+        callbacks=callbacks_list
+    )
 
 
-def get_network(input_size):
+# def get_hot_sequences(raw_input):
+#     notes_sequences = []
+#     lengths_sequences = []
+#     notes_predictions = []
+#     lengths_predictions = []
+#
+#     for i in range(SEQUENCE_LENGTH, len(raw_input)):
+#         notes_sequence = []
+#         lengths_sequence = []
+#
+#         for index in range(i - SEQUENCE_LENGTH, i):
+#             notes_sequence.append(raw_input[index][1])
+#             lengths_sequence.append(raw_input[index][0])
+#         notes_sequences.append(notes_sequence)
+#         lengths_sequences.append(lengths_sequence)
+#
+#         notes_predictions.append(raw_input[i][1])
+#         lengths_predictions.append(raw_input[i][0])
+#
+#     return notes_sequences, lengths_sequences, notes_predictions, lengths_predictions
+#
+#
+# def get_hot_network(input_size):
+#     model = Sequential()
+#     model.add(LSTM(
+#         512,
+#         input_shape=(input_size[1], input_size[2]),
+#         return_sequences=True,
+#         recurrent_activation="sigmoid"
+#         # recurrent_dropout=0.3  - will not run on GPU if not separated
+#     ))
+#     model.add(BatchNormalization())
+#     model.add(Dropout(0.02))
+#
+#     # model.add(LSTM(
+#     #     512,
+#     #     return_sequences=True,
+#     #     # recurrent_dropout=0.3
+#     # ))
+#     # model.add(BatchNormalization())
+#     # model.add(Dropout(0.3))
+#
+#     model.add(LSTM(512))
+#     model.add(BatchNormalization())
+#     model.add(Dropout(0.02))
+#
+#     model.add(Dense(256))
+#     model.add(BatchNormalization())
+#     model.add(Dropout(0.02))
+#
+#     model.add(Dense(128))
+#     model.add(Activation('relu'))     # model.add(Activation('relu' | 'tanh'))
+#     model.add(BatchNormalization())
+#     model.add(Dropout(0.02))
+#
+#     model.add(Dense(64))
+#     model.add(Activation('sigmoid'))  # softmax
+#     model.compile(
+#         loss='categorical_crossentropy',
+#         optimizer='rmsprop'
+#     )
+#
+#     return model
+#
+#
+# def run_hot_features():
+#     notes_input = []
+#     lengths_input = []
+#     notes_output = []
+#     lengths_output = []
+#
+#     for name in os.listdir('../data'):  # or '../sequences' if .npy files loaded
+#         path = os.path.join('../data', name)
+#
+#         try:
+#             # midi_input = np.load(path, allow_pickle=True)  # if .npy files loadeds
+#             midi_input = get_sequence_of_notes(path, Mode.VELOCITIES, False, False)
+#             midi_input = midi_input[0]
+#             notes_sequences, lengths_sequences, notes_predictions, lengths_predictions = get_hot_sequences(midi_input)
+#             notes_input.extend(notes_sequences)
+#             lengths_input.extend(lengths_sequences)
+#             notes_output.extend(notes_predictions)
+#             lengths_output.extend(lengths_predictions)
+#             # print(f'        loaded {name}')
+#
+#         except Exception as ex:
+#             # pass
+#             print(f'failed to load {name}')
+#
+#     ni = np.asarray(notes_input)
+#     ni = ni[:, :, 23:87]  # 27 - 86 in use, 22 - 86 to round to 64
+#     no = np.asarray(notes_output)
+#     no = no[:, 23:87]
+#
+#     # FIND HIGHEST AND LOWEST NOTES
+#     # k_min = 128
+#     # k_max = 0
+#     # for num, s in enumerate(notes_input):
+#     #     print(num)
+#     #     for keys in s:
+#     #         for pos, value in enumerate(keys):
+#     #             if value:
+#     #                 if pos < k_min:
+#     #                     k_min = pos
+#     #                 if pos > k_max:
+#     #                     k_max = pos
+#     #
+#     # print()
+#     # print(k_min)
+#     # print(k_max)
+#
+#     input_size = (len(ni), SEQUENCE_LENGTH, 64)  # notes_input
+#     model = get_hot_network(input_size)
+#     train_network(model, ni, no)  # notes_input, notes_output
+
+def get_music21_network(input_size):
     model = Sequential()
     model.add(LSTM(
         512,
@@ -55,16 +167,15 @@ def get_network(input_size):
     model.add(BatchNormalization())
     model.add(Dropout(0.02))
 
-    model.add(Dense(256))
+    model.add(Dense(128))
     model.add(BatchNormalization())
     model.add(Dropout(0.02))
 
-    model.add(Dense(128))
+    model.add(Dense(32))
     model.add(Activation('relu'))     # model.add(Activation('relu' | 'tanh'))
     model.add(BatchNormalization())
-    model.add(Dropout(0.02))
 
-    model.add(Dense(64))
+    model.add(Dense(10))
     model.add(Activation('sigmoid'))  # softmax
     model.compile(
         loss='categorical_crossentropy',
@@ -73,80 +184,31 @@ def get_network(input_size):
 
     return model
 
+def get_music21_sequences(raw_input):
+    sequences = []
+    predictions = []
 
-def train_network(lstm_model, lstm_input, lstm_output):
-    filepath = 'weights.hdf5'
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
-    )
-    callbacks_list = [checkpoint]
-    lstm_model.fit(
-        lstm_input,
-        lstm_output,
-        epochs=200,         # TODO: 200
-        batch_size=32,      # TODO: 128
-        callbacks=callbacks_list
-    )
+    for i in range(SEQUENCE_LENGTH, len(raw_input)):
+        notes_sequence = []
+        for index in range(i - SEQUENCE_LENGTH, i):
+            notes_sequence.append(raw_input[index])
+
+        sequences.append(notes_sequence)
+        predictions.append(raw_input[i])
+
+    return sequences, predictions
+
+
+def run_music21_features():
+    midi_input = np.load('../sequences/chords21.npy', allow_pickle=True)
+    midi_input = np.transpose(midi_input)
+    sequences, predictions = get_music21_sequences(midi_input)
+    ni = np.asarray(sequences)
+    no = np.asarray(predictions)
+    input_size = (len(ni), SEQUENCE_LENGTH, 10)
+    model = get_music21_network(input_size)
+    train_network(model, ni, no)
 
 
 if __name__ == '__main__':
-    notes_input = []
-    lengths_input = []
-    notes_output = []
-    lengths_output = []
-
-    for name in os.listdir('../data'):  # or '../sequences' if .npy files loaded
-        path = os.path.join('../data', name)
-
-        try:
-            # midi_input = np.load(path, allow_pickle=True)  # if .npy files loadeds
-            midi_input = get_sequence_of_notes(path, Mode.VELOCITIES, False, False)
-            midi_input = midi_input[0]
-            notes_sequences, lengths_sequences, notes_predictions, lengths_predictions = get_sequences(midi_input)
-            notes_input.extend(notes_sequences)
-            lengths_input.extend(lengths_sequences)
-            notes_output.extend(notes_predictions)
-            lengths_output.extend(lengths_predictions)
-            # print(f'        loaded {name}')
-
-        except Exception as ex:
-            # pass
-            print(f'failed to load {name}')
-
-    ni = np.asarray(notes_input)
-    ni = ni[:, :, 23:87]  # 27 - 86 in use, 22 - 86 to round to 64
-    no = np.asarray(notes_output)
-    no = no[:, 23:87]
-
-    # for i in range(len(notes_sequences)):
-    #     for j in range(100):
-    #         notes_sequences[i][j] = notes_sequences[i][j][27:87]
-    #         notes_sequences[i][j][27] = False
-    #
-    # for i in range(len(notes_predictions)):
-    #     notes_predictions[i] = notes_predictions[i][27:87]
-    #     notes_predictions[i][27] = False
-
-    # k_min = 128
-    # k_max = 0
-    # for num, s in enumerate(notes_input):
-    #     print(num)
-    #     for keys in s:
-    #         for pos, value in enumerate(keys):
-    #             if value:
-    #                 if pos < k_min:
-    #                     k_min = pos
-    #                 if pos > k_max:
-    #                     k_max = pos
-    #
-    # print()
-    # print(k_min)
-    # print(k_max)
-
-    input_size = (len(ni), SEQUENCE_LENGTH, 64)  # notes_input
-    model = get_network(input_size)
-    train_network(model, ni, no)   # notes_input, notes_output
+    run_music21_features()
