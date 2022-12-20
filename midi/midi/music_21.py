73b2c95b90abe791fce2fcee291112a46e58cae3
szgorski <101061451+szgorski@users.noreply.github.com>
2022-12-20 01:37:46 +0100

    major factorization of midi package
    Mode.NOTES functionality removed, Mode and Note classes removed, dependencies chnged accardingly, documentation of functions added, other minor refactorizations

--- midi/midi/music_21.py
+++ midi/midi/music_21.py
@@ -1,22 +1,41 @@
-import os
 import numpy as np
 
 from music21 import *
-from decode import Mode, get_sequence_of_notes, export_tempo_array, get_filename, export_output
+from decode import get_sequence_of_notes, export_tempo_array
 
 
-def get_event_lists(f):
-    events = [[] for _ in range(len(f) + 1)]
-    tuples = [None] * len(f)
+def check_number_of_tracks(array, filename, number):
+    """
+    checks if number of tracks equals an expected value: if not, raises a ValueError
 
-    active_tracks = range(len(f))
+    :param array:
+    :param filename:
+    :param number:
+    :return:
+    """
+    if len(array) != number:
+        raise ValueError('number of tracks ({}) in {} not matching the expected value ({})'
+                         .format(len(array), filename, number))
+
+
+def get_event_lists(array):
+    """
+    translates a multi-track array representing notes into a list of events with active notes
+    
+    :param array: 
+    :return: 
+    """
+    events = [[] for _ in range(len(array) + 1)]
+    tuples = [None] * len(array)
+
+    active_tracks = range(len(array))
     active_tracks = list(active_tracks)
     to_remove = []
     removed_tracks = []
 
     for track in active_tracks:
-        if len(f[track]) > 0:
-            tuples[track] = f[track].pop(0)
+        if len(array[track]) > 0:
+            tuples[track] = array[track].pop(0)
         else:
             to_remove.append(track)
             # active_tracks.remove(track)
@@ -29,18 +48,18 @@ def get_event_lists(f):
     while len(active_tracks) > 0:
         d = [tuples[i][0] for i in active_tracks]
         diff = min(d)
-        events[len(f)].append(diff)
+        events[len(array)].append(diff)
 
         for track in active_tracks:
             events[track].append(tuples[track][1])
             tuples[track] = (tuples[track][0] - diff, tuples[track][1])
 
             while tuples[track][0] == 0:
-                if len(f[track]) == 0:
+                if len(array[track]) == 0:
                     to_remove.append(track)
                     break
                 else:
-                    tuples[track] = f[track].pop(0)
+                    tuples[track] = array[track].pop(0)
 
         for track in removed_tracks:
             events[track].append([])
@@ -53,10 +72,19 @@ def get_event_lists(f):
     return events
 
 
-def insert_meta_features(partial_features, events, tempos):
+def insert_meta_features(partial_features, event_lengths, tempos):
+    """
+    processes arrays of tempos and events' lengths,
+    then adds two rows of respective features to already existing list of features
+    
+    :param partial_features: 
+    :param event_lengths: 
+    :param tempos: 
+    :return: 
+    """
     offset = 0
-    while len(events[-1]) > 0:
-        state = events[-1].pop(0)
+    while len(event_lengths[-1]) > 0:
+        state = event_lengths[-1].pop(0)
         partial_features[-2].append(1 / state)  # lengths mapping 1/x
         partial_features[-1].append(min(1.0, 1000 / tempos[offset]))  # tempos mapping 1000/x
         offset += state
@@ -64,59 +92,20 @@ def insert_meta_features(partial_features, events, tempos):
     return partial_features
 
 
-# VERSION WITH OCTAVE/TONE
-def get_tonal_midi_features(events, tempos):
-    features = [[] for _ in range(2 * len(events))]
-    for track in range(len(events) - 1):
-        while len(events[track]) > 0:
-            state = events[track].pop(0)
-
-            if len(state) == 0:
-                features[2 * track + 0].append(0.0)
-                features[2 * track + 1].append(0.0)
-            elif len(state) == 1:
-                features[2 * track + 0].append(max(0.0, min(1.0, (state[0] // 12 - 1) / 8)))  # octaves mapping x/8
-                features[2 * track + 1].append((state[0] % 12 + 1) / 12)  # tones mapping x/12
-            else:
-                ch = chord.Chord(state)
-                ch = ch.root().midi
-                features[2 * track + 0].append(max(0.0, min(1.0, (ch // 12 - 1) / 8)))
-                features[2 * track + 1].append((ch % 12 + 1) / 12)
-
-    features = insert_meta_features(features, events, tempos)
-    return features
-
-
-# VERSION WITH MIDI NOTE
-def get_raw_midi_features(events, tempos):
-    features = [[] for _ in range(len(events) + 1)]
-    for track in range(len(events) - 1):
-        while len(events[track]) > 0:
-            state = events[track].pop(0)
-
-            if len(state) == 0:
-                features[track].append(0.0)
-            elif len(state) == 1:
-                features[track].append(state[0] / 128)  # notes mapping x/128
-            else:
-                ch = chord.Chord(state)
-                ch = ch.root().midi
-                features[track].append(ch / 128)
-
-    features = insert_meta_features(features, events, tempos)
-    return features
-
-
-def check_number_of_tracks(f, filename, number):
-    if len(f) != number:
-        raise ValueError('number of tracks ({}) in {} not matching the expected value ({})'
-                         .format(len(f), filename, number))
-
-
 def preprocess_features(filepath, filename, check_tracks=False, number_of_tracks=0):
+    """
+    gets a tempo array and a list of events from a MIDI file,
+    optionally checks if number of tracks matches an expected value
+
+    :param filepath:
+    :param filename:
+    :param check_tracks:
+    :param number_of_tracks:
+    :return:
+    """
     # file_input = np.load(filepath, allow_pickle=True)  # TODO: choose input method
     # file_input = file_input.tolist()
-    file_input = get_sequence_of_notes(filepath, Mode.BOOLEANS, False, True)
+    file_input = get_sequence_of_notes(filepath, False, False, True)
 
     if check_tracks:
         check_number_of_tracks(file_input, filename, number_of_tracks)
@@ -126,7 +115,75 @@ def preprocess_features(filepath, filename, check_tracks=False, number_of_tracks
     return event_lists, tempo_array
 
 
+# VERSION WITH OCTAVE/TONE
+def get_tonal_midi_features(events, tempos):
+    """
+    calculates the most significant note in each track for every event,
+    translates a list of events and tempos into an array of normalised tonal features of notes (octave and tone)
+
+    :param events:
+    :param tempos:
+    :return:
+    """
+    features_list = [[] for _ in range(2 * len(events))]
+    for track in range(len(events) - 1):
+        while len(events[track]) > 0:
+            state = events[track].pop(0)
+
+            if len(state) == 0:
+                features_list[2 * track + 0].append(0.0)
+                features_list[2 * track + 1].append(0.0)
+            elif len(state) == 1:
+                features_list[2 * track + 0].append(max(0.0, min(1.0, (state[0] // 12 - 1) / 8)))  # octaves mapping x/8
+                features_list[2 * track + 1].append((state[0] % 12 + 1) / 12)  # tones mapping x/12
+            else:
+                ch = chord.Chord(state)
+                ch = ch.root().midi
+                features_list[2 * track + 0].append(max(0.0, min(1.0, (ch // 12 - 1) / 8)))
+                features_list[2 * track + 1].append((ch % 12 + 1) / 12)
+
+    features_list = insert_meta_features(features_list, events, tempos)
+    return features_list
+
+
+# VERSION WITH MIDI NOTE
+def get_raw_midi_features(events, tempos):
+    """
+    calculates the most significant note in each track for every event,
+    translates a list of events and tempos into an array of normalised MIDI notes' features
+
+    :param events:
+    :param tempos:
+    :return:
+    """
+    features_list = [[] for _ in range(len(events) + 1)]
+    for track in range(len(events) - 1):
+        while len(events[track]) > 0:
+            state = events[track].pop(0)
+
+            if len(state) == 0:
+                features_list[track].append(0.0)
+            elif len(state) == 1:
+                features_list[track].append(state[0] / 128)  # notes mapping x/128
+            else:
+                ch = chord.Chord(state)
+                ch = ch.root().midi
+                features_list[track].append(ch / 128)
+
+    features_list = insert_meta_features(features_list, events, tempos)
+    return features_list
+
+
 def get_midi_features(filepath, filename, check_tracks=False, number_of_tracks=0):
+    """
+    gets an array of MIDI notes' features from a MIDI file
+
+    :param filepath:
+    :param filename:
+    :param check_tracks:
+    :param number_of_tracks:
+    :return:
+    """
     event_lists, tempo_array = preprocess_features(filepath, filename, check_tracks, number_of_tracks)
     feature_list = get_raw_midi_features(event_lists, tempo_array)
     feature_list = np.asarray(feature_list)
@@ -136,6 +193,15 @@ def get_midi_features(filepath, filename, check_tracks=False, number_of_tracks=0
 
 
 def get_tonal_features(filepath, filename, check_tracks=False, number_of_tracks=0):
+    """
+    gets an array of tonal features of notes (octave and tone) from a MIDI file
+
+    :param filepath:
+    :param filename:
+    :param check_tracks:
+    :param number_of_tracks:
+    :return:
+    """
     event_lists, tempo_array = preprocess_features(filepath, filename, check_tracks, number_of_tracks)
     feature_list = get_tonal_midi_features(event_lists, tempo_array)
     feature_list = np.asarray(feature_list)
@@ -145,14 +211,17 @@ def get_tonal_features(filepath, filename, check_tracks=False, number_of_tracks=
 
 
 if __name__ == '__main__':
+    import os
+    from decode import get_filename, export_output
+
     for name in os.listdir('../../data'):
         path = os.path.join('../../data', name)
 
         try:
-            features = get_midi_features(path, name)
-            # features = get_tonal_features(path, name)
+            features_array = get_midi_features(path, name)
+            # features_array = get_tonal_features(path, name)
 
-            export_output('../../sequences', get_filename(path), features)
+            export_output('../../sequences', get_filename(path), features_array)
 
         except Exception as ex:
             print(ex)
