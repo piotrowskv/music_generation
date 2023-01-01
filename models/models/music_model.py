2f6435fd18fadd3d4c9675523ca499bfd285b062
Weronika Piotrowska <w.piotrowska00@wp.pl>
2023-01-01 21:54:38 +0100

    added generating sample interface

--- models/models/music_model.py
+++ models/models/music_model.py
@@ -65,3 +65,10 @@ class MusicModel(ABC):
         Loads the model from `path`.
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def generate(self, seed: int | list[int] | None = None) -> Path:
+        """
+        Generates a sample and saves it as a .mid file. Returns path to saved file.
+        """
+        raise NotImplementedError
