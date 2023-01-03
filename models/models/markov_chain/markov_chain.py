40c8783ff54f44cb7070385ff4c54d5c80908b92
Weronika Piotrowska <piotrowska.w00@gmail.com>
2023-01-03 16:19:03 +0100

    edited progress reporting

--- models/models/markov_chain/markov_chain.py
+++ models/models/markov_chain/markov_chain.py
@@ -51,8 +51,8 @@ class MarkovChain(MusicModel):
         for i in range(len(n_gram_next)):
 
             elapsed = time.time() - start
-            progress_callback(SeriesProgress([(i, elapsed)]))
-            
+            progress_callback(SeriesProgress([(elapsed, 100*i/len(n_gram_next))]))
+
             print(str(i) + " " + str(elapsed))
             for j in range(len(n_gram_next[i])):
                 if len(n_gram_next[i]) <= 1:
