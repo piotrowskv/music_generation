712dbf70d2998bd1ed2bd9752ea183c7e2dacf85
shilangyu <xmarcinmarcin@gmail.com>
2023-01-14 21:39:08 +0100

    Remove fullstop from model descriptions

--- backend/app/supported_models.py
+++ backend/app/supported_models.py
@@ -15,15 +15,15 @@ class SupportedModels(Enum):
     LSTM = ModelVariant(
         id='0f5e5af5-9ede-4cc8-814d-f7a0dfb8a6d6',
         name='LSTM',
-        description='Sequential model generating each timestep one by one.')
+        description='Sequential model generating each timestep one by one')
     MARKOV = ModelVariant(
         id='e5893ba5-1cd1-4153-ac56-6b5898897503',
         name='Markov Chain',
-        description='Statistical model generating most probable notes.')
+        description='Statistical model generating most probable notes')
     GAN = ModelVariant(
         id='eafdabd3-fe56-474d-91be-7a9eeeed2124',
         name='GAN',
-        description='Generative model generating the whole song at once.')
+        description='Generative model generating the whole song at once')
 
     @staticmethod
     def from_model_id(id: str) -> SupportedModels | None:
