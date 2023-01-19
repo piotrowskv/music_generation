6505bc48709b648ff84978820b0be75c7a01caad
shilangyu <xmarcinmarcin@gmail.com>
2023-01-19 22:11:59 +0100

    Save models on disk

--- backend/app/training_manager.py
+++ backend/app/training_manager.py
@@ -14,6 +14,8 @@ class TrainingManager:
     _progress_repo: TrainingProgressRepository
     _sessions_repo: TrainingSessionsRepository
 
+    _saved_models_path = Path('saved_models')
+
     def __init__(self, progress_repo: TrainingProgressRepository, sessions_repo: TrainingSessionsRepository):
         self._progress_repo = progress_repo
         self._sessions_repo = sessions_repo
@@ -44,6 +46,7 @@ class TrainingManager:
 
             model.train_on_files(files, 10, on_progress)
 
+            model.save(self._weights_path_for(session_id))
             self._sessions_repo.save_training_progress(session_id, progress)
         except Exception as ex:
             msg = str(ex)
@@ -52,5 +55,32 @@ class TrainingManager:
         finally:
             shutil.rmtree(dir)
 
+    def generate_sample(self, session_id: str, model: MusicModel, seed: int) -> bytes:
+        """
+        Given a model that was previously trained and saved using `add_model` a new midi sample
+        is generated based on a seed. Returns bytes of the midi file.
+        """
+        model.load(self._weights_path_for(session_id))
+
+        midi_bytes: bytes
+
+        # midi package does not support in-memory midi. Midi has to be
+        # materialized into files in the real file system.
+        dir = Path(tempfile.mkdtemp())
+        try:
+            p = dir.joinpath('out.mid')
+            model.generate(p, seed)
+
+            with open(p, mode="rb") as f:
+                midi_bytes = f.read()
+
+        finally:
+            shutil.rmtree(dir)
+
+        return midi_bytes
+
     async def subscribe(self, session_id: str) -> AsyncIterable[ProgressList]:
         return self._progress_repo.subscribe(session_id)
+
+    def _weights_path_for(self, session_id: str) -> Path:
+        return self._saved_models_path.joinpath(session_id)
